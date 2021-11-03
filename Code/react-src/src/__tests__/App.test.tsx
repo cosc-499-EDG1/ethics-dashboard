1f95045fb8287e9b66f89cdebca4f756caa34fa9
Ross <ross.morrison312@gmail.com>
2021-11-03 10:59:11 -0700

    change(react): Move test file & update

--- Code/react-src/src/__tests__/App.test.tsx
+++ Code/react-src/src/__tests__/App.test.tsx
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
-test('renders learn react link', () => {
+test('renders navbar', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Ethics Dashboard/i);
   expect(linkElement).toBeInTheDocument();
 });
