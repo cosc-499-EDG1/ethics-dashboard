a555d516b1064e52c6540dee7302962dc408714d
Ben-Fitz <benfitzharris@aol.com>
2022-02-28 19:45:13 -0800

    Stakeholder Controller
    Stakeholder controller with dashboard state retrieval. Controller only has create function implemented

---
+++ Code/node-src/app/controllers/stakeholder.controller.ts
@@ -0,0 +1,54 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: 'jwt.env' });
+import { Request, Response, NextFunction } from 'express';
+import { useStoreState, State} from "../../../react-src/node_modules/easy-peasy";
+import Stakeholder from "../models/stakeholder.model";
+import Dashboard from '../models/dashboard.model';
+import  {DashboardModel, DashboardData } from '../../../react-src/src/stores/dashboard.store'
+
+export default class StakeholderController {
+
+
+
+    create = async (req: Request, res: Response, next: NextFunction) => {
+        const {title, description, number} = req.body;
+        if (!title || !description || !number) {
+            res.status(400).send({
+                message: 'Invalid form data.',
+            });
+            return;  
+        }
+        const account = req.account;
+        const dash = useStoreState((state: State<DashboardModel>) => state.dashboard_id);
+
+        const stakeholder = new Stakeholder({stakeholder_title: title, stakeholder_desc: description, stakeholder_num: number, dashboard_id: dash});
+        stakeholder.save().then(async () => {
+            res.status(200).json({ message: 'Stakeholder created successfully.', success: true});
+        })
+        .catch(err => {
+            res.status(400).json({
+                message: 'Error creating stakeholder' , 
+            });
+        });
+    };
+
+    findAll = async (req: Request, res: Response, next: NextFunction) => {
+        //TODO: IMPLEMENT
+        return res.sendStatus(200);
+    };
+
+    findOne = async (req: Request, res: Response, next: NextFunction) => {
+
+    };
+
+    update = async (req: Request, res: Response, next: NextFunction) => {
+        //TODO: IMPLEMENT
+        return res.sendStatus(200);
+    };
+
+    delete = (req: Request, res: Response, next: NextFunction) => {
+        //TODO: IMPLEMENT
+        return res.sendStatus(200);
+    };
+}
+
---
+++ Code/react-src/src/stores/dashboard.store.ts
@@ -0,0 +1,37 @@
+import AccountService, { AccountResponse } from "../services/account.service";
+
+import { Action, action, Computed, computed, Thunk, thunk, useStoreState, State } from "easy-peasy";
+import Dashboard from "../../../node-src/build/models/dashboard.model"
+import { AccountModel } from "./account.store";
+import Account from "../../../node-src/build/models/account/account.model";
+
+export interface DashboardData {
+    dashboard_id: number;
+    owner_id: number;
+};
+
+export interface DashboardModel {
+    dashboard_id: number | null;
+    owner_id: number | null;
+    setDashboardId: Action<DashboardModel, number>;
+    setOwnerId: Action<AccountModel, Account>;
+
+}
+
+
+const dashboardStore: DashboardModel = {
+    dashboard_id: null,
+    owner_id: null,
+
+    setDashboardId: action((state, payload) => {
+        state.dashboard_id = payload;
+    }),
+
+    setOwnerId: action((state, payload) => {
+        state.account = payload;
+    })
+
+};
+
+
+export default dashboardStore;
