ed3044e35768eab614b1d0a17e03ce0601f304fe
Ben-Fitz <benfitzharris@aol.com>
2022-03-01 19:09:59 -0800

    options
    Added option controller and route just with the create function

---
+++ Code/node-src/app/controllers/option.controller.ts
@@ -0,0 +1,51 @@
+import dotenv from 'dotenv';
+dotenv.config({path: 'jwt.env'});
+import { Request, Response, NextFunction } from 'express';
+import { useStoreState, State} from "../../../react-src/node_modules/easy-peasy";
+import option from "../models/option.model";
+import Dashboard from '../models/dashboard.model';
+import  {DashboardModel, DashboardData } from '../../../react-src/src/stores/dashboard.store'
+
+class OptionController {
+
+    create = async (req: Request, res: Response, next: NextFunction) => {
+        const {title, description, number} = req.body;
+        if (!title || !description || !number) {
+            res.status(400).send({
+                message: 'Invalid form data.',
+            });
+            return;  
+        }
+        const dash = useStoreState((state: State<DashboardModel>) => state.dashboard_id);
+
+        const opt = new option({option_title: title, option_desc: description, option_num: number, dashboard_id: dash});
+        opt.save().then( async () => {
+            res.status(200).json({ message: 'Stakeholder created successfully.', success: true});
+        }).catch(err => {
+            res.status(400).json({
+                message: 'Error creating stakeholder' , 
+            });
+        });
+    };
+
+    findAll = async (req: Request, res: Response, next: NextFunction) => {
+        //TODO: IMPLEMENT
+        return res.sendStatus(200);
+    };
+
+    findOne = async (req: Request, res: Response, next: NextFunction) => {
+
+    };
+
+    update = async (req: Request, res: Response, next: NextFunction) => {
+        //TODO: IMPLEMENT
+        return res.sendStatus(200);
+    };
+
+    delete = (req: Request, res: Response, next: NextFunction) => {
+        //TODO: IMPLEMENT
+        return res.sendStatus(200);
+    };
+}
+
+export default new OptionController();
