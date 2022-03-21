bcd34b61e6bad7f7862cd654221a5f6464fdb238
Michael Turner <michaeltt009@gmail.com>
2022-03-21 14:58:06 -0700

    Fixed the add stakeholder option on the stakeholder  page

--- Code/react-src/src/components/global/stakeholderform.tsx
+++ Code/react-src/src/components/global/stakeholderform.tsx
@@ -6,7 +6,9 @@ type StakeholderFormProps = {
   isLoading: boolean;
   message: string;
   wasSuccess: boolean;
+  numStakeholders: number;
   onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+  setNumStakeholders?: any;
 };
 
 export const StakeholderForm = ({
@@ -15,8 +17,21 @@ export const StakeholderForm = ({
   isLoading,
   message,
   wasSuccess,
+  numStakeholders,
   onSubmit,
+  setNumStakeholders,
 }: StakeholderFormProps) => {
+  const changeNumStakeholders = async (increase: boolean) => {
+    if (increase === true) {
+      if (numStakeholders < 9) {
+        setNumStakeholders(numStakeholders+1);
+      }
+    } else {
+      if (numStakeholders > 3) {
+        setNumStakeholders(numStakeholders-1);
+      }
+    }
+  };
   return (
     <form
       onSubmit={(e) => {
@@ -42,6 +57,16 @@ export const StakeholderForm = ({
       {inputs.map((input, i) => {
         return <React.Fragment key={i}>{input}</React.Fragment>;
       })}
+      <div className="text-center justify-center">
+        <button className="bg-primary hover:brightness-110 text-white text-lg font-bold py-2 mx-2 w-40 rounded-md focus:outline-none focus:shadow-outline"
+        onClick={() => changeNumStakeholders(true)}>
+          Add Option
+        </button>
+        <button className="bg-primary hover:brightness-110 text-white text-lg font-bold py-2 mx-2 w-40 rounded-md focus:outline-none focus:shadow-outline"
+        onClick={() => changeNumStakeholders(false)}>
+          Remove Option
+        </button>
+      </div>
       <div className="pt-4 flex items-center justify-center">
         {actions.map((action, i) => {
           return <React.Fragment key={i}>{action}</React.Fragment>;
