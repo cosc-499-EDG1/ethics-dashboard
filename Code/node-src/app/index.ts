ffe63fc062930ed7133889e16218d579464bced4
Ross <ross.morrison312@gmail.com>
2022-03-29 11:41:12 -0700

    Change: Add test dashboard data

--- Code/node-src/app/index.ts
+++ Code/node-src/app/index.ts
@@ -38,5 +38,30 @@ setImmediate(async () => {
             VALUES (${adminResult[0]}, ${classResult[0]});
             `;
         await db.query(instructorRelationInsert, { type: QueryTypes.INSERT });
+
+        const dashboardInsert = `
+            INSERT IGNORE INTO Dashboards (name, ownerId, summary, dilemmas, role)
+            VALUES ('Test Dashboard', ${userResult[0]}, 'Testing Case Summary', 'Testing Dilemmas', 'Testing Role');
+            `;
+        const dashboardResult = await db.query(dashboardInsert, { type: QueryTypes.INSERT });
+
+        const dashboardOptionInsert = `
+            INSERT IGNORE INTO CaseOptions(option_title, option_desc, option_num, dashboard_id)
+            VALUES ('Test Option 1', 'Testing Option 1', 1, ${dashboardResult[0]}),
+            ('Test Option 2', 'Testing Option 2', 2, ${dashboardResult[0]}),
+            ('Test Option 3', 'Testing Option 3', 3, ${dashboardResult[0]});
+            `;
+        await db.query(dashboardOptionInsert, { type: QueryTypes.INSERT });
+
+        const stakeholderInsert = `
+            INSERT IGNORE INTO Stakeholders (title, description, num, dashboard_id)
+            VALUES ('Test Stakeholder 1', 'Testing Stakeholder 1', 1, ${dashboardResult[0]}),
+            ('Test Stakeholder 2', 'Testing Stakeholder 2', 2, ${dashboardResult[0]}),
+            ('Test Stakeholder 3', 'Testing Stakeholder 3', 3, ${dashboardResult[0]}),
+            ('Test Stakeholder 4', 'Testing Stakeholder 4', 4, ${dashboardResult[0]}),
+            ('Test Stakeholder 5', 'Testing Stakeholder 4', 4, ${dashboardResult[0]}),
+            ('Test Stakeholder 6', 'Testing Stakeholder 4', 4, ${dashboardResult[0]});
+        `;
+        await db.query(stakeholderInsert, { type: QueryTypes.INSERT });
     }
 });
